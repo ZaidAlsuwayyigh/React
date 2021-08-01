264c18739f41a9daf3033a4cac437797a545c8ac
Zaid Alsuwayyigh <z.alsuwayyigh-t@tamkeentech.sa>
2021-08-01 22:36:57 +0300

    This branch focuses on different ways of implementing conditions. There are three different ways in this branch

--- Projects/firstproject/src/components/Expenses/Expenses.js
+++ Projects/firstproject/src/components/Expenses/Expenses.js
@@ -9,23 +9,74 @@ const Expenses = (props) => {
     const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
     }
+    
     const filteredExpenses = props.items.filter(expense => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
+
+   /* --------------------------------------------------- */
+    /*3- Start: third way to use if statement */
+     // let expenseContent = <p style={{color: 'white'}}>No content for this year!</p>
+    // if(filteredExpenses.length > 0){
+    //     expenseContent =  filteredExpenses.map((expense) => (
+    //         <ExpenseItem 
+    //         key= {expense.id}
+    //         title={expense.title} 
+    //         amount={expense.amount} 
+    //         date={expense.date} 
+    //         />
+    //         ));
+    // };
+
+    /*3- Start: third way to use if statement */
+    /* --------------------------------------------------- */
+    
     return (
         <div>
              
              <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onSelectedYear={filterChangeHandler} />
-
-                {filteredExpenses.map((expense) => (
+                 {/* --------------------------------------------------- */}
+                {/*1-  Start: One way to use if statement */}
+                {filteredExpenses.length > 0 ? filteredExpenses.map((expense) => (
                 <ExpenseItem 
                 key= {expense.id}
                 title={expense.title} 
                 amount={expense.amount} 
                 date={expense.date} 
                 />
-                ))}
+                )) :
+                 <p style={{color: 'white'}}>No content for this {filteredYear}</p>
+                }
+
+                 {/*1- End:  One way to use if statement */}
+
+                 {/* --------------------------------------------------- */}
+
+                {/* --------------------------------------------------- */}
+                 {/*2- Start: second way to use if statement */}
+
+                {/* {filteredExpenses.length === 0 && <p style={{color: 'white'}}>No content for this year!</p>}
+
+                {filteredExpenses.length > 0 && filteredExpenses.map((expense) => (
+                <ExpenseItem 
+                key= {expense.id}
+                title={expense.title} 
+                amount={expense.amount} 
+                date={expense.date} 
+                />
+                ))} */}
+
+                 {/*2- End: second way to use if statement */}
+                 {/* --------------------------------------------------- */}
+
+                {/* --------------------------------------------------- */}
+                 {/*3- Start: third way to use if statement */}
+
+                    {/* {expenseContent} */}
+
+                 {/*3- End: second way to use if statement */}
+                 {/* --------------------------------------------------- */}
              </Card>
 
         </div>
