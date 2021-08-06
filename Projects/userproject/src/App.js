6999eb2e62ed504588677cbfb879ec51a0d31a31
Zaid Alsuwayyigh <z.alsuwayyigh-t@tamkeentech.sa>
2021-08-06 17:15:37 +0300

    A small project showing how to use useState, forms, and to pass data from child to a parent component

--- Projects/userproject/src/App.js
+++ Projects/userproject/src/App.js
@@ -1,9 +1,22 @@
+import React, { useState } from 'react';
+import AddUser from './Component/Users/AddUser';
+import UsersList from './Component/Users/UsersList';
+
+
 
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const getInfoFromAddUser = (uName, uAge) => {
+    setUsersList( prevUsersList => {
+      return [...prevUsersList, { name: uName, age: uAge, id: Math.random().toString() }];
+    });
+  };
   return (
-    <div className="App">
-      <h2>User develop</h2>
+    <div>
+      <AddUser  liftStateToApp={getInfoFromAddUser}/>
+      <UsersList users={usersList}/>
     </div>
   );
 }
---
+++ Projects/userproject/src/Component/Users/AddUser.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+
+import Card from '../UI/Card';
+import Button from '../UI/Button';
+import classes from './AddUser.module.css';
+
+
+const AddUser = (props) => {
+  const [currentUserName, setCurrentUserName] = useState('');
+  const [userAge, setUserAge] = useState('');
+
+  const handleName = (event) => {
+    setCurrentUserName(event.target.value);
+  }
+
+  const handleAge = (event) => {
+      setUserAge(event.target.value);
+  }
+
+  const handleFormSubmit = (event) => {
+      event.preventDefault(); 
+
+      if(userAge <= 0){
+       alert(`Dear ${currentUserName}, make sure the age is a proper one. THank you.`);
+        
+        return;
+      }
+
+      if(currentUserName.length <= 2){
+        alert(`UserName must contain more than two characters!! ${currentUserName.length}`);
+        return;
+      }
+      props.liftStateToApp(currentUserName, userAge);
+
+
+
+      setCurrentUserName('');
+      setUserAge('');
+  }
+
+  
+
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor="username" >Username</label> 
+        <input id="username" type="text" value={currentUserName} onChange={handleName}  />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" value={userAge} onChange={handleAge}  />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
