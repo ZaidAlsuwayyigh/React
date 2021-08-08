d497133ec655ace8e422e757510445be13f49590
Zaid Alsuwayyigh <z.alsuwayyigh-t@tamkeentech.sa>
2021-08-08 15:05:26 +0300

    Edited few things, but most importantly, i used useRef instaed of useState for inputs to extract the values and have less code/lines. However, useRef can be used to read the value without changing it. that is why i used it.

--- Projects/userproject/src/components/UI/ErrorModel.js
+++ Projects/userproject/src/components/UI/ErrorModel.js
@@ -1,25 +1,40 @@
 import React from 'react';
+import ReactDom from 'react-dom';
 
 import Card from './Card';
 import Button from './Button';
 import classes from './ErrorModel.module.css';
 
-const ErrorModel = (props) => {
+const Backdrop = (props) => {
+  return  <div className={classes.backdrop} onClick={props.onConfirm} />;
+  
+}
+
+const ModelOverlay = (props) => {
   return (
-    <div>
-      <div className={classes.backdrop} onClick={props.onConfirm} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onConfirm}>Okay</Button>
-        </footer>
-      </Card>
-    </div>
+    <Card className={classes.modal}>
+  <header className={classes.header}>
+    <h2>{props.title}</h2>
+  </header>
+  <div className={classes.content}>
+    <p>{props.message}</p>
+  </div>
+  <footer className={classes.actions}>
+    <Button onClick={props.onConfirm}>Okay</Button>
+  </footer>
+</Card>
+  )
+}
+const ErrorModel = (props) => {
+  
+  return (
+    <React.Fragment>
+      {ReactDom.createPortal(<Backdrop  onConfirm={props.onConfirm}/>, 
+       document.getElementById('backdrop-root'))}  
+
+     {ReactDom.createPortal(<ModelOverlay title={props.title} message={props.message} onConfirm={props.onConfirm} />, 
+      document.getElementById('overlay-root'))}
+    </React.Fragment>
   );
 };
 
--- Projects/userproject/src/components/Users/AddUser.js
+++ Projects/userproject/src/components/Users/AddUser.js
@@ -1,18 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import ErrorModel from '../UI/ErrorModel';
 import classes from './AddUser.module.css';
+import Wrapper from '../Helpers/Wrapper';
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+  // const [enteredUsername, setEnteredUsername] = useState('');
+  // const [enteredUserAge, setEnteredUserAge] = useState('');
   const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    const enteredName = nameInputRef.current.value;
+    const enteredAge = ageInputRef.current.value;
+    if (enteredName.trim().length === 0 || enteredAge.trim().length === 0) {
       setError({
         title: 'Invalid input',
         message: 'Please enter a valid name and age (non-empty values).',
@@ -26,25 +31,27 @@ const AddUser = (props) => {
       });
       return;
     }
-    props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername('');
-    setEnteredAge('');
+    props.onAddUser(enteredName, enteredAge);
+    // setEnteredUsername('');
+    // setEnteredUserAge('');
+    nameInputRef.current.value = '';
+    ageInputRef.current.value = '';
   };
 
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  };
+  // const usernameChangeHandler = (event) => {
+  //   setEnteredUsername(event.target.value);
+  // };
 
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
-  };
+  // const ageChangeHandler = (event) => {
+  //   setEnteredUserAge(event.target.value);
+  // };
 
   const errorHandler = () => {
     setError(null);
   };
 
   return (
-    <div>
+    <Wrapper>
       {error && (
         <ErrorModel
           title={error.title}
@@ -58,20 +65,18 @@ const AddUser = (props) => {
           <input
             id="username"
             type="text"
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
+            ref={nameInputRef}
           />
           <label htmlFor="age">Age (Years)</label>
           <input
             id="age"
             type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
+            ref={ageInputRef}
           />
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
